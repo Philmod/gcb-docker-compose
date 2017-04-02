e78340a2e1894423fcd0b6b8b0dbd459cf506a6f
Philmod <philippe.modard@gmail.com>
2017-04-02 11:23:00 -0400

    unique integration test

--- test/integration_test.js
+++ test/integration_test.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const grpc = require('grpc');
 const should = require('should');
+const async = require('async');
 
 const PORT = process.env.PORT || 50051;
 const PROTO_PATH = path.join(__dirname, '..', 'counter.proto');
@@ -8,66 +9,33 @@ const counterProto = grpc.load(PROTO_PATH);
 
 describe('integration tests', () => {
 
-  let server;
   let client;
 
   before(() => {
-    // Start server.
-    server = require('../index.js').server;
-  });
-
-  before(() => {
     // Create a grpc client.
+    // This assumes that the server is running.
     client = new counterProto.counter.CounterService(
       'localhost:' + PORT,
       grpc.credentials.createInsecure()
     );
+    console.log('yo : ', client);
   });
 
-  after(() => {
-    server.forceShutdown();
+  it('should reset, then add to the counter', (done) => {
+    async.series({
+      reset: function(callback) {
+        client.reset({}, callback);
+      },
+      add: function(callback) {
+        client.add({count: 1}, callback);
+      },
+      get: function(callback) {
+        client.get({}, (err, res) => {
+          should.not.exist(err);
+          res.count.should.equal(1);
+        });
+      }
+    }, done);
   });
-
-  describe('reset', () => {
-
-    beforeEach(done => {
-      client.add({count: 1}, done);
-    });
-
-    it('should reset the counter', (done) => {
-      client.reset({}, (err, res) => {
-        should.not.exist(err);
-        res.count.should.equal(0);
-        done();
-      });
-    });
-
-  });
-
-  describe('add', () => {
-
-    beforeEach(done => {
-      client.reset({}, done);
-    });
-
-    it('should increment the counter', (done) => {
-      client.add({count: 1}, (err, res) => {
-        should.not.exist(err);
-        res.count.should.equal(1);
-        done();
-      });
-    });
-
-    it('should decrement the counter', (done) => {
-      client.add({count: -1}, (err, res) => {
-        should.not.exist(err);
-        res.count.should.equal(-1);
-        done();
-      });
-    });
-
-  });
-
-  // Maybe add a test to prove than restarting the server doesn't restart the counter.
 
 });
