a557201ac5c1af5b51363584567d5d10a7bbef8d
Philmod <philippe.modard@gmail.com>
2017-04-02 19:27:20 -0400

    use redis as a backend for the counter; fakeredis module is used for testing

--- index.js
+++ index.js
@@ -1,37 +1,52 @@
 const path = require('path');
 const grpc = require('grpc');
+const redis = require('redis');
 
 const NODE_ENV = process.env.NODE_ENV || 'development';
 const PORT = process.env.PORT || 50051;
+
+// Redis client.
+const createRedisClient = (env) => {
+  const opts = {
+    host: process.env.REDIS_PORT_6379_TCP_ADDR || '127.0.0.1',
+    port: process.env.REDIS_PORT_6379_TCP_PORT || 6379
+  };
+  if (env === 'test') {
+    return require('fakeredis').createClient(opts);
+  }
+  return require('redis').createClient(opts);
+}
+const redisClient = createRedisClient(NODE_ENV);
+const counterRedisKey = 'gcb-docker-compose-counter'
+
+// grpc proto.
 const PROTO_PATH = path.join(__dirname, 'counter.proto');
-
 const counterProto = grpc.load(PROTO_PATH);
-
 let server = new grpc.Server();
 
-let counter = 0;
-
+// Methods.
 const add = (call, callback) => {
-  counter += call.request.count;
-  const res = {
-    count: counter
-  };
-  callback(null, res);
+  redisClient.incrby(counterRedisKey, call.request.count, (err, counter) => {
+    callback(err, {
+      count: counter
+    });
+  });
 };
 
 const reset = (call, callback) => {
-  counter = 0;
-  const res = {
-    count: counter
-  };
-  callback(null, res);
+  redisClient.set(counterRedisKey, 0, (err, counter) => {
+    callback(err, {
+      count: 0
+    });
+  });
 };
 
 const get = (call, callback) => {
-  const res = {
-    count: counter
-  };
-  callback(null, res);
+  redisClient.get(counterRedisKey, (err, counter) => {
+    callback(err, {
+      count: counter
+    });
+  });
 };
 
 const methods = {
@@ -40,10 +55,9 @@ const methods = {
   get: get,
 };
 
-if (NODE_ENV === 'test') {
-  module.exports = methods;
-} else {
-  server.addProtoService(counterProto.counter.CounterService.service, methods);
-  server.bind('0.0.0.0:' + PORT, grpc.ServerCredentials.createInsecure());
-  server.start();
-}
+// Start grpc server.
+server.addProtoService(counterProto.counter.CounterService.service, methods);
+server.bind('0.0.0.0:' + PORT, grpc.ServerCredentials.createInsecure());
+server.start();
+
+module.exports = methods;
